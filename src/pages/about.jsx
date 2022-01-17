87b7a3e71990f2e93ac6604431d4230d3a4e7947
Rita Mak <ritapmak@gmailcom>
2022-01-17 12:31:52 +0000

    about page changed

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,7 +11,7 @@ Developer with a background in Event Production (I used to be an event
 producer until the covid-19 appeared...)`,
   },
   {
-    title: "Why web development?",
+    title: "Why web development",
     description: `Well, I was working in the event industy but suddenly I found myself at
   home, with no job and no clue of what to do next. Do nothing is never an
   option for me! That was when I discovered the wonderful world of web
